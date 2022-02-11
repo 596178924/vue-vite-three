669b774399810839b3cb1927a60fa2d081530e48
metaDragonMaster <2327139286@qq.com>
2022-02-11 16:29:57 +0800

    first

---
+++ .history/src/App_20220211161235.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    vue vite three
+    <p class="p-1">vue p
+
+      <span>span</span>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
+
+
+<style lang="scss">
+
+</style>
---
+++ .history/src/App_20220211161259.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    vue vite three
+    <p class="p-1">vue p
+
+      <span>span</span>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
+
+
+<style lang="scss">
+.p-1 {
+  color: red;
+  span {
+    color: aqua;
+  }
+}
+</style>
